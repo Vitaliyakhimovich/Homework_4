1a9b9565a219383a374a0397b2755f1377cf7ebf
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-27 16:48:15 +0300

    add Exaple 001

---
+++ Example001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadInt("Введите число A ");
+int numberB = ReadInt("Введите число B ");
+stepen(numberA, numberB);
+
+
+void stepen(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
