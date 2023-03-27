7bbbffe5d56577ac5371b34e91cabb079209b7cd
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-27 17:06:02 +0300

    add Example 003

---
+++ Example003/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+
+int[] mas = new int [8];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 9);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+FillArray(mas);
+PrintArray(mas);
