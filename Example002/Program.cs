95aee82222b496267cc9e3991c246e26903d349c
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-27 17:02:55 +0300

    add Example 003

---
+++ Example002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int d = 0;
+int sum = 0;
+
+while(num > 0)
+{
+d = num % 10;
+num = num / 10;
+sum = sum + d ;
+}
+
+Console.WriteLine($"Сумма чисел: {sum}");
